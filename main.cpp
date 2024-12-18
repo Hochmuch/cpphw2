8917707651f1f5ccf7111b54f5ff75614240f28e
Hochmuch <egorrussian56@gmail.com>
2024-12-18 23:02:27 +0300

    minor changes

--- main.cpp
+++ main.cpp
@@ -2,6 +2,6 @@
 #include "fluid.h"
 
 int main() {
-    Fluid<Fixed<32, 8>, Fast_fixed<32, 8>, Fixed<32, 8>, 36, 84> fluid;
+    Fluid<Fixed<32, 8>, Fixed<32, 8>, Fixed<32, 8>, 36, 84> fluid;
     fluid.run();
 }
